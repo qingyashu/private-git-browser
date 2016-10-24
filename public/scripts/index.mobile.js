12335ddabab239ee3ce38183abbd4dcbe7018acd
Qingya Shu <shu_q@worksap.co.jp>
2016-10-24 16:59:46 +0800

    add jquery mobile, complete footer and header

--- public/scripts/index.mobile.js
+++ public/scripts/index.mobile.js
@@ -8,6 +8,7 @@ if (sessionStorage.getItem('login') !== 'true') {
 var app = angular.module('listApp', []);
 
 app.controller('listCtrl', function($scope, $http) {
+  $scope.currentPath = '';
   $http({
     method: 'GET',
     url: '/ls',
@@ -21,8 +22,7 @@ app.controller('listCtrl', function($scope, $http) {
       $scope.filelist.push({
         filename: file.filename, 
         path: file.path,
-        type: file.type,
-        children: []
+        type: file.type
       });
     }
     console.log($scope.filelist);
@@ -31,33 +31,26 @@ app.controller('listCtrl', function($scope, $http) {
   $scope.clickFile = function(fileObj, eventObj) {
     console.log(fileObj);
     if (fileObj.type === 'dir') {
-      if (fileObj.expanded === true) { // need collapse 
-        fileObj.children = [];
-        fileObj.expanded = false;
-      }
-      else { // need expand
-        $http({
-          method: 'GET',
-          url: '/ls',
-          params: {
-            path: fileObj.path + '/' + fileObj.filename
-          }
-        })
-        .then(function(response) {
-          console.log(response.data);
-          fileObj.children = [];
-          for (var file of response.data) {
-            fileObj.children.push({
-              filename: file.filename, 
-              path: file.path, 
-              type: file.type,
-              children: []
-            });
-          }
-          fileObj.expanded = true;
-          console.log(fileObj);
-        });
-      }
+      $http({
+        method: 'GET',
+        url: '/ls',
+        params: {
+          path: fileObj.path + '/' + fileObj.filename
+        }
+      })
+      .then(function(response) {
+        $scope.currentPath = $scope.currentPath + '/' + fileObj.filename;
+        console.log(response.data);
+        $scope.filelist = [];
+        for (var file of response.data) {
+          $scope.filelist.push({
+            filename: file.filename, 
+            path: file.path, 
+            type: file.type
+          });
+        }
+
+      });
     }
     else {
       $http({
